2f5fae879a0c9276ef3260562dd4e58d826f7b51
Kristian S. Munkvold <ksm@computas.com>
2016-02-03 14:55:01 +0100

    Setup for Assignment 2

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
+
 
 app.use('/', express.static('client'));
 
-app.listen(3000, function callback () {
+app.listen(port, function callback () {
     console.log('Running our app at http://localhost:3000')
 });
