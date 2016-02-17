c4fcb51d772884f40ebfb8140a78390ebc48cd86
Nicolas Almagro Tonne <nixatto@gmail.com>
2016-02-18 00:03:13 +0100

    Graph

--- client/client.js
+++ client/client.js
@@ -1,5 +1,7 @@
 var socket = io();
 var first = true;
+var max_display_len = 200;
+var chart;
 
 socket.on('connect', function (data) {
     console.log("I am connected");
@@ -18,6 +20,63 @@ function itemToRow(item){
         + '</td><td>' + time + '</td></tr>';
 }
 
+function init(data){
+
+    var tr = '';
+    var stop_at = data.length - max_display_len;
+    var upload_series = [];
+    var download_series = [];
+    var ping_series = [];
+
+    for (var i = 0; i < data.length - 1; i++) {
+        var d = data[i];
+        var dd = d.date;
+
+        tr += itemToRow(d);
+        download_series.push([dd, d.download]);
+        upload_series.push([dd, d.upload]);
+        ping_series.push([dd, d.ping]);
+
+    };
+
+    $('#history').prepend(tr);
+    $("#history-table").tablesorter();
+
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'graph',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData
+            }
+        },
+        title: {
+            text:""
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            title: {
+                text: 'MB/s'
+            }
+        },
+        series: [{
+            name: 'Download',
+            data: download_series
+        }, {
+            name: 'Upload',
+            data: upload_series
+        }]
+    });
+}
+
+function requestData(){
+
+}
+
 
 socket.on('client:display', function (data) {
     var last = $(data).get(-1);
@@ -27,15 +86,25 @@ socket.on('client:display', function (data) {
     $("#ping").text(Math.floor(last.ping));
 
     if(first){
-        var tr = '';
-        for (var i = data.length - 1; i >= 0; i--) {
-            tr += itemToRow(data[i]);
-        };
-        $('#history').prepend(tr);
-        $("#history-table").tablesorter(); 
+        init(data);
+        first = false;
     }else{
         $('#history').prepend(itemToRow(last));
         $("#history-table").trigger("update"); 
+
+
+        chart.series[0].addPoint([last.date, last.download], true, false);
+        chart.series[1].addPoint([last.date, last.upload], true, false);
+
+        /*
+
+        var series = chart.series[0],
+        shift = series.data.length > 20;
+        // add the point
+        
+
+        */
+
     }
 
     
--- logger/logger.js
+++ logger/logger.js
@@ -3,6 +3,7 @@ var fileSystem = require('fs');
 var fileName = __dirname + '/history.json';
 var history = JSON.parse(fileSystem.readFileSync(fileName));
 var socket = require('socket.io-client')(process.argv[2] || 'http://localhost:3000');
+var num_speedtests = 0;
 
 socket.on('connect', function () {
     console.log('Logger is connected');
@@ -13,32 +14,37 @@ socket.on('logger:history', function () {
 });
 
 socket.on('logger:run', function () {
-    console.log('Starting speedtest...');
-    
-    var test = speedtest();
+    if(num_speedtests < 1){
+        num_speedtests++;
+        console.log('Starting speedtest...');
+        
+        var test = speedtest();
 
-    test.on('data', function (data) {
-        var result = {
-            download: data.speeds.download,
-            upload: data.speeds.upload,
-            ping: data.server.ping,
-            date: Date.now()
-        };
+        test.on('data', function (data) {
+            var result = {
+                download: data.speeds.download,
+                upload: data.speeds.upload,
+                ping: data.server.ping,
+                date: Date.now()
+            };
 
-        history.push(result);
-        socket.emit('server:results', history);
+            history.push(result);
+            socket.emit('server:results', history);
 
-        var jsonResult = JSON.stringify(history);
-        fileSystem.writeFile(fileName, jsonResult, function (err) {
-            if (err) {
-                console.log('Something went wrong: ' + err);
-            } else {
-                console.log('Speedtest finished');
-            }
+            var jsonResult = JSON.stringify(history);
+            fileSystem.writeFile(fileName, jsonResult, function (err) {
+                if (err) {
+                    console.log('Something went wrong: ' + err);
+                } else {
+                    console.log('Speedtest finished');
+                }
+            });
+            num_speedtests--;
         });
-    });
 
-    test.on('error', function (err) {
-        console.error(err);
-    });
+        test.on('error', function (err) {
+            console.error(err);
+            num_speedtests--;
+        });
+    }
 });
