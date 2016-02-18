d3f3a56ec606f6da9f7ee0750e7bd75493649abb
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-02-18 20:50:48 +0100

    helloe

--- client/highscore.js
+++ client/highscore.js
@@ -11,6 +11,8 @@ socket.on('client:display', function (results) {
         tbody[i].innerHTML = '';
     }
 
+    var tr = document.getElementsByTagName("tr");
+
     function getMax(arr, prop) {
         var max;
         for (var i=0 ; i<arr.length ; i++) {
@@ -36,32 +38,65 @@ socket.on('client:display', function (results) {
     console.log(maxU);
     console.log(minP);
 
+    var pingtr = document.createElement("tr");
+    var downloadtr = document.createElement("tr");
+    var uploadtr = document.createElement("tr");
 
+    function addToCorrectHighscore(){
 
+    }
 
-    results.forEach(function (result) {
-
-        var tr = document.createElement("tr");
+    for(i = 0; i < tr.length; i++){
+        var rank = document.createElement("td")
         var date = document.createElement("td");
-        var ping = document.createElement("td")
-        var download = document.createElement("td")
-        var upload = document.createElement("td")
-        var ip = document.createElement("td")
-        var bestDate = document.createElement("td");
-        var bestPing = document.createElement("td")
-        var bestDownload = document.createElement("td")
-        var bestUpload = document.createElement("td")
-        var bestIp = document.createElement("td")
+        var ping = document.createElement("td");
+        var download = document.createElement("td");
+        var upload = document.createElement("td");
+        var ip = document.createElement("td");
+        if(i == 0){
+            rank.textContent = "1";
+            ip.textContent = minP.ip;
+            date.textContent = (new Date(minP.date)).toLocaleString();
+            ping.textContent = minP.ping;
 
-        date.textContent = (new Date(result.date)).toLocaleString();
-        ping.textContent = result.ping;
-        download.textContent = result.download;
-        upload.textContent = result.upload;
-        ip.textContent = result.ip;
+            tr[i].appendChild(rank);
+            tr[i].appendChild(ping);
+            tr[i].appendChild(ip);
+            tr[i].appendChild(date);
 
+            tbody[i].appendChild(tr[i]);
 
+        }
+        else if(i == 1){
+            rank.textContent = "1";
+            ip.textContent = maxD.ip;
+            date.textContent = (new Date(maxD.date)).toLocaleString();
+            download.textContent = maxD.download;
 
+            tr[i].appendChild(rank);
+            tr[i].appendChild(download);
+            tr[i].appendChild(ip);
+            tr[i].appendChild(date);
 
-    });
+            tbody[i].appendChild(tr[i]);
+
+        }
+        else if(i == 2){
+            rank.textContent = "1";
+            ip.textContent = maxU.ip;
+            date.textContent = (new Date(maxU.date)).toLocaleString();
+            upload.textContent = maxU.upload;
+
+            tr[i].appendChild(rank);
+            tr[i].appendChild(upload);
+            tr[i].appendChild(ip);
+            tr[i].appendChild(date);
+
+            tbody[i].appendChild(tr[i]);
+
+        }
+
+    }
+    
 
 });
