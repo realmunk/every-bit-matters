29c274b4efbeacf6196417d49483e85832bd19cd
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-02-18 16:14:53 +0100

    helloe

---
+++ client/highscore.js
@@ -0,0 +1,37 @@
+var socket = io();
+
+socket.on('connect', function (data) {
+    console.log("I am connected");
+});
+
+socket.on('client:display', function (results) {
+
+    var tbody = document.getElementsByTagName('tbody')[0];
+    tbody.innerHTML = '';
+
+    results.forEach(function (result) {
+        // add table row
+        var tr = document.createElement('tr');
+        // add columns
+        var rank = document.createElement('td');
+        var ping = document.createElement('td');
+        var download = document.createElement('td');
+        var upload = document.createElement('td');
+        var ip = document.createElement('td');
+        // add content to columns
+        date.textContent = (new Date(result.date)).toLocaleString();
+        ping.textContent = result.ping;
+        download.textContent = result.download;
+        upload.textContent = result.upload;
+        ip.textContent = result.ip;
+        // append columns to row
+        tr.appendChild(date);
+        tr.appendChild(ping);
+        tr.appendChild(download);
+        tr.appendChild(upload);
+        tr.appendChild(ip)
+        // append row to tbody
+        tbody.appendChild(tr);
+    });
+
+});
