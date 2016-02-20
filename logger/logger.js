2a31024ed934921355bf1537caa99511413876db
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-02-20 15:40:01 +0100

    Tried to fix highscore

--- logger/logger.js
+++ logger/logger.js
@@ -23,7 +23,7 @@ socket.on('logger:run', function () {
             upload: data.speeds.upload,
             ping: data.server.ping,
             date: Date.now(),
-            ip: data.client.ip,
+            ip: data.client.ip
         };
 
         history.push(result);
