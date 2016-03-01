bedcf2cf740f8b4082a46223f1e26b092b79ecdb
Didrik Pemmer Aalen <didrikpa@gmail.com>
2016-03-01 14:17:27 +0100

    map

--- client/client.js
+++ client/client.js
@@ -1,18 +1,94 @@
 var socket = io();
+var marks = [];
+var markers = [];
+var first = 0;
 
 socket.on('connect', function (data) {
     console.log("I am connected");
     console.log(document.getElementsByTagName("tr")[0].id)
 });
 
-socket.on('client:display', function (results) {
-    console.log(results[0].lat);
-    var latlon = {lat: results[0].lat,  lng: results[0].lon};
-    var marker = new google.maps.Marker({
-        position: latlon,
-        map: map,
-        name: results[0].ip
+
+
+function initMap() {
+    var myLatLng = {lat: 63.363, lng: 10.044};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: myLatLng
     });
+
+}
+
+function setMarker(mark){
+
+
+}
+
+function addMarker(bool){
+    var tbody = document.getElementsByTagName('tbody')[0];
+    tbody.innerHTML = '';
+    if(bool == true){
+        var tr = document.createElement("tr");
+        var ip = document.createElement('td');
+        var ping = document.createElement('td');
+        var download = document.createElement('td');
+        var upload = document.createElement('td');
+        ip.textContent = marks[marks.length-1].ip;
+        ping.textContent = marks[marks.length-1].ping;
+        download.textContent = marks[marks.length-1].download;
+        upload.textContent = marks[marks.length-1].upload;
+        tr.appendChild(ip);
+        tr.appendChild(ping);
+        tr.appendChild(download);
+        tr.appendChild(upload);
+        tbody.appendChild(tr);
+        setMarker(marks[marks.length-1]);
+    }
+    else{
+        var table = document.getElementById("myTable");
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            if(row.cell[0] === marks[bool].ip){
+                row.cell[1] = marks[bool].ping;
+                row.cell[2] = marks[bool].download;
+                row.cell[3] = marks[bool].upload;
+            }
+
+        }
+
+    }
+
+
+
+
+}
+
+function checkLastTest(test) {
+    var counter = 0;
+    for (var i = 0; i < marks.length; i++) {
+        if (marks[0].ip != test.ip) {
+            counter += 1;
+        }
+        else{
+            marks[i].ip = test.ip;
+            return i;
+        }
+    }
+    if (counter === marks.length) {
+        marks.push(test);
+        return true;
+    }
+
+}
+
+
+
+socket.on('client:display', function (results) {
+    if(results.length > 1){
+        var lastTest = $(results).get(-1);
+        addMarker(checkLastTest(lastTest));
+
+    }
 
     /*
     var tbody = document.getElementsByTagName('tbody')[0];
@@ -27,7 +103,6 @@ socket.on('client:display', function (results) {
         var download = document.createElement('td');
         var upload = document.createElement('td');
         var ip = document.createElement('td');
-        var mac = document.createElement("td");
         // add content to columns
         date.textContent = (new Date(result.date)).toLocaleString();
         ping.textContent = result.ping;
@@ -46,12 +121,3 @@ socket.on('client:display', function (results) {
 
 });
 
-function initMap() {
-    var myLatLng = {lat: 63.363, lng: 10.044};
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: myLatLng
-    });
-
-}
