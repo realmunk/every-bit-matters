5076f63dbd014905ee4f71364ecad2bbc90d766c
Kristian S. Munkvold <ksm@computas.com>
2016-03-29 18:20:02 +0200

    Get that client.

--- client/client.js
+++ client/client.js
@@ -4,6 +4,6 @@ socket.on('connect', function (data) {
     console.log("I am connected");
 });
 
-socket.on('logger:display', function (data) {
+socket.on('client:display', function (data) {
     console.log(data);
 });
